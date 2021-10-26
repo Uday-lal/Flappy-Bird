37cf3aef6ee10acacb0edee8a1de9acec9dd2ccd
Uday-lal <udaylal014@gmail.com>
2021-10-27 00:44:45 +0530

    Add the backgroud sprite

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import kaboom from "kaboom";
 
-kaboom({ debug: true });
+kaboom();
 
 // Loading the sprites
-loadSprite("bg", "./sprites/bg.png");
-add([sprite("bg")]);
+loadRoot(
+  "https://raw.githubusercontent.com/Uday-lal/Flappy-Bird/master/src/sprites/"
+);
+
+loadSprite("bg", "bg.png");
+add([sprite("bg"), scale(4.8, 1.5), { width: width(), height: height() }]);
